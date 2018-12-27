55ee6c97740b1c8c5b7ae7280fc5127fda9c0ff4
KarasawaTakahiro <karasawa@basicinc.jp>
2018-12-27 19:08:51 +0900

    add PhaseType

---
+++ phase/phase_type/phase_type.go
@@ -0,0 +1,34 @@
+package PhaseType
+
+type PhaseType int
+
+const (
+	Opened PhaseType = iota
+	Labeled
+	Unlabeled
+	Pushed
+	ForcePushed
+	Closed
+	Reopend
+)
+
+func (p PhaseType) String() string {
+	switch p {
+	case Opened:
+		return "Opened"
+	case Labeled:
+		return "Labeled"
+	case Unlabeled:
+		return "Unlabeled"
+	case Pushed:
+		return "Pushed"
+	case ForcePushed:
+		return "ForcePushed"
+	case Closed:
+		return "Closed"
+	case Reopend:
+		return "Reopend"
+	default:
+		return "Unknown"
+	}
+}
