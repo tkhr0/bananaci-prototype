3f0dfda2d4f9a3f9fa8b2aca7772105980346efd
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2019-01-01 22:24:09 +0900

    add server

---
+++ server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	jobQueue "github.com/tkhr0/bananaci-prototype/job_queue"
+	"github.com/tkhr0/bananaci-prototype/runtime"
+)
+
+func Call(maxWorkers int, maxQueues int) {
+	d := jobQueue.NewDispatcher(maxWorkers, maxQueues)
+
+	sigHandler(d)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			d.Add(generator())
+		}
+	}()
+
+	go func() {
+		for {
+			fmt.Printf("pool: %d queue: %d\n", d.Pool(), d.Queue())
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	println("start")
+	d.Start()
+	println("wait")
+	d.Wait()
+	println("exit")
+}
+
+func sigHandler(d *jobQueue.Dispatcher) {
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGUSR1, syscall.SIGINT)
+
+	go func() {
+		for {
+			switch s := <-sigCh; s {
+			case syscall.SIGUSR1:
+				d.Add(generator())
+			case syscall.SIGINT:
+				d.Stop(true)
+				return
+			}
+		}
+	}()
+}
+
+func generator() jobQueue.Job {
+	return *jobQueue.NewJob(*runtime.NewRuntime())
+}
