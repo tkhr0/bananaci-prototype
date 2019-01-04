9bae122d5236e6e0ed5605fce9118a00af7310eb
KarasawaTakahiro <karasawa@basicinc.jp>
2019-01-04 19:49:44 +0900

    change add job from webhook

--- server/http.go
+++ server/http.go
@@ -5,9 +5,17 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+
+	jobQueue "github.com/tkhr0/bananaci-prototype/job_queue"
+	"github.com/tkhr0/bananaci-prototype/phase"
+	"github.com/tkhr0/bananaci-prototype/runtime"
 )
 
-func CallHTTP() {
+var dispatcher *jobQueue.Dispatcher
+
+func CallHTTP(d *jobQueue.Dispatcher) {
+	dispatcher = d
 
 	httpHandler()
 
@@ -19,6 +27,7 @@ func CallHTTP() {
 
 func httpHandler() {
 	http.HandleFunc("/endpoint", pathEndpoint)
+	http.HandleFunc("/new", pathNew)
 
 }
 
@@ -32,5 +41,32 @@ func pathEndpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(len(params))
 	for k, param := range params {
 		fmt.Printf("%s: %s\n", k, param)
+	}
+}
+
+func pathNew(w http.ResponseWriter, r *http.Request) {
+	for k, params := range parseQuery(r) {
+		for _, param := range params {
+			switch k {
+			case "opened":
+				n, err := strconv.Atoi(param)
+				if err != nil {
+					continue
+				}
+				for i := 0; i < n; i++ {
+					dispatcher.Add(*jobQueue.NewJob(*runtime.NewRuntime()))
+				}
+			case "labeled":
+				n, err := strconv.Atoi(param)
+				if err != nil {
+					continue
+				}
+				for i := 0; i < n; i++ {
+					r := *runtime.NewRuntime()
+					r.Phase = phase.NewOpened().ToLabeled()
+					dispatcher.Add(*jobQueue.NewJob(*runtime.NewRuntime()))
+				}
+			}
+		}
 	}
 }
